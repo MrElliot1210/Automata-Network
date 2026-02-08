28014da77f189a871f2f2b59599ade0b0cd767e6
MrElliot1210 <shahmahfuzali@hotmail.co.uk>
2026-02-08 14:50:43 +0000

    Implemented concatenation
    and fixed visualisation

--- concatenation.js
+++ concatenation.js
@@ -6,8 +6,64 @@ function concatenation() {
     const nfa1 = savedNFAs[name1];
     const nfa2 = savedNFAs[name2];
 
-    console.log(nfa1);
-    console.log(nfa2);
+    const newNFA = {
+        states: [],
+        alphabet: Array.from(new Set([...nfa1.alphabet, ...nfa2.alphabet])),
+        transitionFunction: {},
+        initialState: nfa1.initialState + "A",
+        finalStates: []
+    }
+
+    // add a "A" suffix to all states of nfa1
+    for (const state of nfa1.states) {
+        newNFA.states.push(state + "A");
+
+        newNFA.transitionFunction[state + "A"] = {};
+        for (const symbol of nfa1.alphabet) {
+            newNFA.transitionFunction[state + "A"][symbol] = [];
+            for (const transition of nfa1.transitionFunction[state][symbol]) {
+                newNFA.transitionFunction[state + "A"][symbol].push(transition + "A");
+            }
+        }
+        newNFA.transitionFunction[state + "A"]["\u03B5"] = [];
+        for (const transition of nfa1.transitionFunction[state]["\u03B5"]) {
+            newNFA.transitionFunction[state + "A"]["\u03B5"].push(transition + "A");
+        }
+    }
+
+    // add a "B" suffix to all states of nfa2
+    for (const state of nfa2.states) {
+        newNFA.states.push(state + "B");
+
+        newNFA.transitionFunction[state + "B"] = {};
+        for (const symbol of nfa2.alphabet) {
+            newNFA.transitionFunction[state + "B"][symbol] = [];
+            for (const transition of nfa2.transitionFunction[state][symbol]) {
+                newNFA.transitionFunction[state + "B"][symbol].push(transition + "B");
+            }
+        }
+        newNFA.transitionFunction[state + "B"]["\u03B5"] = [];
+        for (const transition of nfa2.transitionFunction[state]["\u03B5"]) {
+            newNFA.transitionFunction[state + "B"]["\u03B5"].push(transition + "B");
+        }
+    }
+    // keep the final states of nfa2
+    for (const finalState of nfa2.finalStates) {
+        newNFA.finalStates.push(finalState + "B");
+    }
+
+    // add transitions for concatenation
+    for (const finalState of nfa1.finalStates) {
+        newNFA.transitionFunction[finalState + "A"]["\u03B5"].push(nfa2.initialState + "B");
+    }
+
+    // save new NFA
+    let newName = prompt("Enter the name of the new NFA made from '" + name1 + "' \u22C5 '" + name2 + "'.");
+    if (newName != null) {
+        savedNFAs[newName] = newNFA;
+        localStorage.setItem("NFAs", JSON.stringify(savedNFAs));
+        window.location.assign('index.html');
+    }
 }
 
 // generate interface to select two NFAs for concatenation
